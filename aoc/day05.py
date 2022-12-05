d47f2f3ca1fd59dc9f89e4260748e95120461bf7
Fatemeh Moghaddam <fatemeh.moghaddam9696@gmail.com>
2022-12-05 13:47:37 +0100

    day05

---
+++ aoc/day05.py
@@ -0,0 +1,50 @@
+import re
+with open("day05.txt", "rt") as input_file:
+    crates = input_file.readlines()
+
+
+def rearrangement(crates):
+    stacks = [[] for i in range(9)]
+    part_1 = ''
+    part_2 = ''
+    flag = True
+    for line in crates:
+        if line == "\n": continue
+        if line.find("move") == -1:
+            ## this is the positions of crates
+            positions = [(pos.start()+1) for pos in re.finditer('\[', line)]
+            columns = [p//4 for p in positions]
+            for i in range(len(columns)):
+                stacks[columns[i]].append(line[positions[i]])
+        else:
+            if flag:
+                flag = False
+                stacks2 = [s[::-1] for s in stacks]
+                stacks = [s[::-1] for s in stacks]
+            ## extract numbers from instructions
+            [boxes, fr, to] = list(map(int, re.findall('[0-9]+', line)))
+            n = len(stacks2[fr-1])
+            for _ in range(boxes):
+                stacks[to-1].append(stacks[fr-1].pop())
+                stacks2[to-1].append(stacks2[fr-1].pop(n-boxes))
+                
+    for s1, s2 in zip(stacks, stacks2):
+        part_1 += s1.pop()
+        part_2 += s2.pop()
+    return part_1, part_2
+        
+
+### tests
+test_input = ["    [D] ",
+"[N] [C] ",
+"[Z] [M] [P]",
+ " 1   2   3 ", 
+ "move 1 from 2 to 1",
+ "move 3 from 1 to 3",
+ "move 2 from 2 to 1",
+ "move 1 from 1 to 2 "]
+   
+
+
+
+print(rearrangement(crates))

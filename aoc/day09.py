6e89fd189caba5118f78e7e83d321e764d0b0a66
Fatemeh Moghaddam <fatemeh.moghaddam9696@gmail.com>
2023-01-31 12:10:19 +0100

    day09, edited

--- aoc/day09.py
+++ aoc/day09.py
@@ -2,6 +2,9 @@ with open("day09.txt", "rt") as input_file:
     inputs = [line.strip() for line in input_file]
 
 
+# Based on Planck lengths rules; 
+# diagonally adjacent and even overlapping both count as touching
+# input is a head position, and output is a set of allowed positions for tail
 def allowed_pos(pos):
     x,y = pos
     allowed = set()
@@ -9,6 +12,7 @@ def allowed_pos(pos):
         for j in range(-1,2):
             allowed.add((x+i,y+j))
     return allowed
+
 
 
 def move_head(pos, dir):
@@ -19,43 +23,61 @@ def move_head(pos, dir):
     return pos
 
 
-def move_tail(pos, head_pos, seen):
+# output is the new position of the tail
+def move_tail(pos, head_pos):
     x, y = pos
     hx, hy = head_pos
     delta_x, delta_y = hx-x, hy-y
+    # we want to move diagonaly if there was a diagonal difference, 
+    # but we will only take one step at a time, so it should be divided by the amplitude 
+    # also, we need to keep the direction, so divide by the absolute of amplitude
     if delta_x != 0: xmove = delta_x//abs(delta_x)
     else: xmove = 0
     if delta_y != 0: ymove = delta_y//abs(delta_y)
     else: ymove = 0
     pos = (x + xmove, y + ymove)
-    seen.add(pos)
     return pos
 
-# part 1
+
+
+## part 1
+# input is not splitted yet
 def safe_rope(movements):
     head_pos = (0,0)
     tail_pos = (0,0)
-    seen = set(tail_pos)
+    # at first I initialized this with (0,0), but for some reason that I don't know,
+    # it stored that as 0 and it stores (0,0) at some point, 
+    # so the number of length was one more than it should
+    seen = set()
     for action in movements:
         [dir, steps] = action.split()
+        # move head one step, check tail, move tail if needed,
+        # do this until all steps are done
         for _ in range(int(steps)):
             head_pos = move_head(head_pos, dir)
             if tail_pos not in allowed_pos(head_pos):
-                tail_pos = move_tail(tail_pos, head_pos, seen)
+                tail_pos = move_tail(tail_pos, head_pos)
+                seen.add(tail_pos)
+    # print((0,0) in seen)
     return seen
 
-# part 2
+
+## part 2
 def long_rope(movements):
     knots = [(0,0) for _ in range(10)]
     seen_target = [set(knots[i]) for i in range(10)]
     for action in movements:
         [dir, steps] = action.split()
         for _ in range(int(steps)):
+            # this is the head
             knots[0] = move_head(knots[0], dir)
             seen_target[0].add(knots[0])
+            # move first knot based on head, then
+            # move each tail based on the tail knot before it
             for i in range(1,10):
                 if knots[i] not in allowed_pos(knots[i-1]):
-                    knots[i] = move_tail(knots[i], knots[i-1], seen_target[i])
+                    knots[i] = move_tail(knots[i], knots[i-1])
+                    seen_target[i].add(knots[i])
     return seen_target
 
 
@@ -97,10 +119,10 @@ test_input2 = ['R 5', 'U 8', 'L 8','D 3', 'R 17', 'D 10', 'L 25', 'U 20']
 
 
 # part 1
-print(len(safe_rope(inputs)))
+print("Part 1: ", len(safe_rope(inputs)))
 # part 2
 seen = long_rope(inputs)
-print(len(seen[-1]))
+print("Part 2: ", len(seen[-1]))
 
 
 
